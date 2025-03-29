fb322e5784ba43e19288a7bd9aecd15e0505a734
Emily Chang <emily0109.ec@gmail.com>
2025-03-29 17:00:45 -0400

    set up

--- frontend/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",  // Scan all files in src folder for Tailwind CSS classes
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
