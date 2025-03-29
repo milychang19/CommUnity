4c9a61cd66e1d6306e91d00800111100242d437f
Emily Chang <emily0109.ec@gmail.com>
2025-03-29 16:20:15 -0400

    setting up react and tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // Scan all files in src folder for Tailwind CSS classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
